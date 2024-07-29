7990a7f11fa57f54eff3b2ce4a016ec235ff7830
felipe <felipefortu33@gmail.com>
2024-07-29 20:52:59 -0300

    Editando CSS deixando mais moderno com melhor EX.

--- edit_task.php
+++ edit_task.php
@@ -43,6 +43,7 @@ $conn->close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Editar Tarefa</title>
 </head>
 <body>
